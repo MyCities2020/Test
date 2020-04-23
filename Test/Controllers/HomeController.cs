2cc5cabdbafc5915e8d9ce7b03b2d82891d9c1cc
Paolo Socrates <paul_socrate@hotmail.fr>
2020-04-23 11:35:20 +0200

    Ajoutez des fichiers projet.

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            LocationLists model = new LocationLists();
+            var locations = new List<Locations>()
+            {
+                new Locations(1, "Bhubaneswar","Bhubaneswar, Odisha", 48.85341, 2.3488),
+                new Locations(2, "Hyderabad","Hyderabad, Telengana", 17.387140, 78.491684),
+                new Locations(3, "Bengaluru","Bengaluru, Karnataka", 12.972442, 77.580643)
+            };
+            model.LocationList = locations;
+            return View(model);
+        }
+    
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
